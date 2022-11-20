e8dc3e64ab69e2595fe4f8fc2bcc253bb6d64ade
Peter-John Thompson <pj_thompson@outlook.com>
2022-11-20 04:31:53 -0500

    Creating Settings menu
    Need to finish light and dark mode functionality and work on notifications

---
+++ app/src/main/java/com/example/swen3001_project_starvationprevention/SettingsFragment.kt
@@ -0,0 +1,11 @@
+package com.example.swen3001_project_starvationprevention
+
+import android.os.Bundle
+import androidx.preference.PreferenceFragmentCompat
+
+class SettingsFragment : PreferenceFragmentCompat() {
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.root_preferences, rootKey)
+    }
+}
