280776b1ca9761fb41de7f35411023b8dffba1f8
Over-Leveraged <kielyjwalsh@gmail.com>
2022-11-30 07:52:28 -0500

    Implemented Decrement and Increment buttons on ItemFragment

--- app/src/main/java/com/example/swen3001_project_starvationprevention/fragment/ItemFragment.kt
+++ app/src/main/java/com/example/swen3001_project_starvationprevention/fragment/ItemFragment.kt
@@ -6,12 +6,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import com.example.swen3001_project_starvationprevention.R
 import com.example.swen3001_project_starvationprevention.databinding.ItemFragmentBinding
 
 class ItemFragment : Fragment() {
 
-
+    private val args by navArgs<ItemFragmentArgs>()
     private var _binding: ItemFragmentBinding? = null
 
     // This property is only valid between onCreateView and
@@ -22,13 +25,57 @@ class ItemFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        //TextView itemDescription = (TextView)findViewById(R.id.itemDescription);
+
         super.onCreate(savedInstanceState)
         _binding = ItemFragmentBinding.inflate(inflater, container, false)
-        var itemDescription = _binding?.itemDescription;
-        itemDescription?.text = "This is a test description"
+        var itemName = _binding?.itemName;
+        itemName?.text = (args.currentItem.item_name)
+
+        var itemPrice = _binding?.FoodPrice;
+        itemPrice?.text = (args.currentItem.item_price.toString())
+
+
         //binding.itemDescription.text = "This is a test"
         
+
+        //Button Creations and Navigation
+        var Incrementbutton = _binding!!.increment
+        var Decrementbutton = _binding!!.decrement
+        var itemQuantity = _binding!!.quantityDisplay
+        //Increment Item cost per click
+        Incrementbutton.setOnClickListener {
+            //Increment item quantity per click
+            var quantity = itemQuantity.text.toString().toInt()
+            quantity++
+            itemQuantity.text = quantity.toString()
+            //Increment item cost per click
+            var cost = _binding!!.totalval.text.toString().toDouble()
+            cost += args.currentItem.item_price
+            _binding!!.totalval.text = cost.toString()
+            //toast to say cost incremented by item price
+            Toast.makeText(context, "Cost Incremented by " + args.currentItem.item_price, Toast.LENGTH_SHORT).show()
+
+        }
+        //Decrement Item cost per click
+        Decrementbutton.setOnClickListener {
+            //Decrement item quantity per click
+            var quantity = itemQuantity.text.toString().toInt()
+            if (quantity > 0) {
+                quantity--
+                itemQuantity.text = quantity.toString()
+                //Decrement item cost per click
+                var cost = _binding!!.totalval.text.toString().toDouble()
+                cost -= args.currentItem.item_price
+                _binding!!.totalval.text = cost.toString()
+                //toast to say cost decremented by item price
+                Toast.makeText(context, "Cost Decremented by " + args.currentItem.item_price, Toast.LENGTH_SHORT).show()
+            }
+
+            var backButton = _binding!!.backButton
+            backButton.setOnClickListener {
+                findNavController().navigate(R.id.action_itemFragment_to_restaurantFragment)
+            }
+        }
 
         return binding.root
 
